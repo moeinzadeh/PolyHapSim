533b29dc163733c3baaea7bf26bdcb37ce09ae44
moeinzadeh <7212838+moeinzadeh@users.noreply.github.com>
2020-01-31 14:05:41 +0100

    Update set_env_variables.sh

--- set_env_variables.sh
+++ set_env_variables.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash 
-export FAINX=/project/moeinzadeh_tools/conda/python3_7conda/bin/faidx
-export HAPLOGENERATOR=/home/muzychen/muzychenko/haplosim/tools/Haplosim/haplogenerator.py
-export MINIMAP2=/project/moeinzadeh_tools/TOOLS/miniConda/bin/minimap2
-export ART_ILLUMINA=/home/muzychen/muzychenko/haplosim/tools/art_bin_MountRainier/art_illumina
-export BWA=/home/muzychen/muzychenko/haplosim/tools/bwa/bwa
-export PICARD=/home/muzychen/muzychenko/haplosim/tools/picard.jar
-export SAMTOOLS=/usr/local/package/bin/samtools
-export HAPCOMPASS=/home/muzychen/muzychenko/haplosim/tools/hapcompass_v0.8.2/hapcompass.jar
-export HPOP=/home/muzychen/muzychenko/haplosim/tools/H-PoPG/H-PoPGv0.2.0.jar
-export SDHAP=/home/muzychen/muzychenko/haplosim/tools/SDhaP/hap
-export RANBOW=/project/moeinzadeh-data/muzychenko/haplosim/tools/ranbow/ranbow_git/ranbow.py
-export SAMPLE_FASTA=/project/moeinzadeh-data/muzychenko/haplosim/sampleFasta/sample.fasta
-
-# check if we are being sourced by another script or shell
+export FAINX=PATHTO/faidx
+export HAPLOGENERATOR=PATHTO/haplogenerator.py
+export MINIMAP2=PATHTO/minimap2
+export ART_ILLUMINA=/PATHTO/art_illumina
+export BWA=PATHTO/bwa
+export PICARD=PATHTO/picard.jar
+export SAMTOOLS=PATHTO/samtools
+export HAPCOMPASS=PATHTO/hapcompass.jar
+export HPOP=PATHTO/H-PoPGv0.2.0.jar
+export SDHAP=PATHTO/hap
+export RANBOW=PATHTO/ranbow.py
+export SAMPLE_FASTA=PATHTO/sample.fasta
 
